9cd658f36fb43ee143be708b44c5108829ac4905
assistantej2pmultiservices-cmd <assistantej2pmultiservices@gmail.com>
2026-09-11 14:11:15 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,82 @@
+const CACHE_NAME = "pointage-v1";
+
+const FILES = [
+  "./",
+  "./index.html"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+
+  );
+
+  self.skipWaiting();
+
+});
+
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys =>
+
+      Promise.all(
+
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+
+      )
+
+    )
+
+  );
+
+  self.clients.claim();
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+  if(event.request.method !== "GET"){
+    return;
+  }
+
+  event.respondWith(
+
+    fetch(event.request)
+      .then(response => {
+
+        const copie =
+          response.clone();
+
+        caches.open(CACHE_NAME)
+          .then(cache => {
+
+            cache.put(
+              event.request,
+              copie
+            );
+
+          });
+
+        return response;
+
+      })
+      .catch(() => {
+
+        return caches.match(
+          event.request
+        );
+
+      })
+
+  );
+
+});
