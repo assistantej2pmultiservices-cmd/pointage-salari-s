877ee07bafb0ecbad3c04eb5e964e01e39ec3afe
assistantej2pmultiservices-cmd <assistantej2pmultiservices@gmail.com>
2026-09-11 14:52:51 -0400

    forcer le nouveau cache hors connexion

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pointage-v2";
+const CACHE_NAME = "pointage-v3";
 
 const FILES = [
   "./",
@@ -6,27 +6,39 @@ const FILES = [
 ];
 
 self.addEventListener("install", event => {
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(FILES))
   );
 
   self.skipWaiting();
+
 });
 
+
 self.addEventListener("activate", event => {
+
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
         keys
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
-      )
-    )
+
+      );
+
+    })
+
   );
 
   self.clients.claim();
+
 });
+
 
 self.addEventListener("fetch", event => {
 
@@ -35,6 +47,7 @@ self.addEventListener("fetch", event => {
   }
 
   event.respondWith(
+
     caches.match(event.request)
       .then(cachedResponse => {
 
@@ -45,22 +58,39 @@ self.addEventListener("fetch", event => {
         return fetch(event.request)
           .then(response => {
 
-            if (response && response.status === 200) {
-              const copie = response.clone();
+            if (
+              response &&
+              response.status === 200
+            ) {
+
+              const copie =
+                response.clone();
 
               caches.open(CACHE_NAME)
                 .then(cache => {
-                  cache.put(event.request, copie);
+
+                  cache.put(
+                    event.request,
+                    copie
+                  );
+
                 });
+
             }
 
             return response;
+
           })
           .catch(() => {
-            return caches.match("./index.html");
+
+            return caches.match(
+              "./index.html"
+            );
+
           });
 
       })
+
   );
 
 });
